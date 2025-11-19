dd7d9ba7dbbfd5ec9d29e6457e97ff3384bf164f
nikhisable5050 <nikhilsable5050@gmail.com>
2025-11-19 16:31:17 +0530

    Added OOP-based Pizza Bill Generator implementation

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+//        Pizza basePizza = new Pizza(true);
+//        basePizza.addExtraCheese();
+//        basePizza.addExtraToppings();
+//        basePizza.getBill();
 
+
+        DeluxPizza dp = new DeluxPizza(false);
+        dp.addExtraCheese();
+        dp.addExtraCheese();
+        dp.takeAway();
+        dp.getBill();
     }
 }
